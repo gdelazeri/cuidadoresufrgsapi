79bb49a37f1a8538aac550e6d7c81fef23088323
guilherme.delazeri <guilherme.delazeri@cwi.com.br>
2020-08-07 11:58:00 -0300

    Inclusão de validações no cadastro de usuário

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,11 @@ const UserService = require('../services/userService');
 class UserController {
   static async post(req, res) {
     try {
-      const user = await UserService.post(req.body);
-      res.send({ user });
+      const response = await UserService.post(req.body);
+      if (response.success) {
+        return res.status(200).send(response);
+      }
+      return res.status(400).send(response);
     } catch (e) {
       log.error(e);
       res.sendStatus(500);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,40 @@
-const log = require('../config/log');
 const UserModel = require('../models/user');
+const ErrorTypes = require('../types/ErrorTypes');
 
 class UserService {
   static async post(body) {
     try {
+      // Check required fields
+      if (!body.name || !body.email || !body.password) {
+        return {
+          success: false,
+          errors: [ErrorTypes.U003],
+        }
+      }
+      
+      // Check if e-mail already exists
+      const emailExistent = await UserModel.getByEmail(body.email);
+      if (emailExistent) {
+        return {
+          success: false,
+          errors: [ErrorTypes.U001],
+        }
+      }
+
+      // Add new user
       const user = await UserModel.add(body);
-      return user;
+
+      if (user) {
+        return {
+          success: true,
+          result: user,
+        }
+      }
+
+      return {
+        success: false,
+        errors: [ErrorTypes.U000],
+      }
     } catch (error) {
       throw error;
     }
