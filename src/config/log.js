133758c5d99550d99f1eec9c885d01f4a65c81b6
guilherme.delazeri <guilherme.delazeri@cwi.com.br>
2020-08-17 11:43:50 -0300

    Alteração para utilizar winston novamente

--- src/config/log.js
+++ src/config/log.js
@@ -6,49 +6,38 @@ const levels = {
   info: 2,
 };
 
-// winston.configure({
-//   exitOnError: false,
-//   levels,
-//   colors: {
-//     error: 'red',
-//     warning: 'yellow',
-//     info: 'green',
-//   },
-//   transports: [
-//     new (winston.transports.Console)({
-//       colorize: true,
-//     }),
-//     new winston.transports.File({
-//       filename: '../log',
-//       name: 'log',
-//       json: false,
-//       maxsize: 10485760, // 1 MB
-//       maxFiles: 5,
-//     }),
-//   ],
-// });
+winston.configure({
+  exitOnError: false,
+  levels,
+  colors: {
+    error: 'red',
+    warning: 'yellow',
+    info: 'green',
+  },
+  transports: [
+    new (winston.transports.Console)({
+      colorize: true,
+    }),
+  ],
+});
 
 
 class Log {
 
   static error(...args) {
-    console.error(`${new Date()} ${args}`);
-    // winston.error(`${new Date()} ${args}`);
+    winston.error(`${new Date()} ${args}`);
   }
 
   static warning(...args) {
-    console.warn(`${new Date()} ${args}`);
-    // winston.warning(`${new Date()} ${args}`);
+    winston.warning(`${new Date()} ${args}`);
   }
 
   static info(...args) {
-    console.log(`${new Date()} ${args}`);
-    // winston.info(`${new Date()} ${args}`);
+    winston.info(`${new Date()} ${args}`);
   }
 
   static database(...err) {
-    console.log(...err);
-    // winston.error(...err);
+    winston.error(...err);
   }
 }
 
