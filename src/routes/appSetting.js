10ecc77f2dc31e36892c0398f303deae3b697da5
guilherme.delazeri <guilherme.delazeri@cwi.com.br>
2020-10-07 23:06:14 -0300

    Removed Auth

--- src/routes/appSetting.js
+++ src/routes/appSetting.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const AppSettingController = require('../controllers/appSettingController');
-const Auth = require('../middlewares/auth');
 
 const router = express.Router();
 
